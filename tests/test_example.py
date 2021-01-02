daab08fae06729cbc8bef0d8fbaedb23f7f8d245
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2021-01-02 00:13:04 -0500

    fix flake

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,10 +1,11 @@
 import unittest
 import lewis_crawler.src.example as example
 
+
 class TestExample(unittest.TestCase):
 
     def test_math(self):
-        result = example.add(2,4)
+        result = example.add(2, 4)
         self.assertEqual(result, 6)
 
 
